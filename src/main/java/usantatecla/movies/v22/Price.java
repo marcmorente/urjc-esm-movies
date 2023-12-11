ab62b795556a0295b9413e3126a92e0807388e61
Marc Morente <mmorente@inthinking.co.uk>
2023-12-11 13:33:31 +0100

    Refactoring 6: [Price] Remove duplicated constant

--- src/main/java/usantatecla/movies/v22/Price.java
+++ src/main/java/usantatecla/movies/v22/Price.java
@@ -3,6 +3,8 @@ package usantatecla.movies.v22;
 public abstract class Price {
 
 	private static final int FREQUENT_RENTER_POINTS = 1;
+
+	protected static final double EXTRA_CHARGE = 1.5;
 	
 	public abstract double getCharge(int daysRented);
 	
